e22a0d5248455fa03ccafe991984e70cafe5a031
Stepan <stepan.dubrov@gmqil.com>
2023-10-03 19:41:20 +0300

    ktor test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("io.ktor.plugin") version "2.3.4"
 }
 
 group = "org.collect.invest"
@@ -8,10 +10,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")}
+
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core-jvm:2.3.4")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
 }
 
 tasks.test {
