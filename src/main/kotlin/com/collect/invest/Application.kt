cfe7a854b8f517fc78b3f869a5cc189b91e93431
Stepan <stepan.dubrov@gmail.com>
2023-10-15 23:29:23 +0300

    base

--- src/main/kotlin/com/collect/invest/Application.kt
+++ src/main/kotlin/com/collect/invest/Application.kt
@@ -6,10 +6,10 @@ import io.ktor.server.netty.*
 import com.collect.invest.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+//    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+//        .start(wait = true)
 }
 
-fun Application.module() {
-    configureRouting()
-}
+//fun Application.module() {
+//    configureRouting()
+//}
